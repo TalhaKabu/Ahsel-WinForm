5ec070040a8a39971dfb1445b5509bc0022f7f78
TalhaKabu <98604259+TalhaKabu@users.noreply.github.com>
2023-06-12 19:16:15 +0300

    asd

--- Ahsel.Winform/Entities/ClientDto.cs
+++ Ahsel.Winform/Entities/ClientDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,6 @@ namespace Ahsel.Winform.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string asd { get; set; }
     }
 }
